16264c744edc4f92c86df1337d5e319d3b74107a
HelenaCla <helenagon96@gmail.com>
2025-01-25 09:14:44 +0100

    cambios en main

--- nou.php
+++ nou.php
@@ -1,3 +1,3 @@
 <?php
-echo "Hola, este es el fichero nou.php en la rama develop. Este es el segundo texto. ";
+echo "Hola, este es el fichero nou.php en la rama main. Este es el segundo texto. ";
 ?>
